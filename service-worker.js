4c62afe1c21914ef2df74bc7b84a5ce90c8924a7
Brandi Neal <nealb3@nku.edu>
2026-07-27 15:00:08 -0400

    Modernize marketplace discovery experience

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='golfer-goodies-v1';
+const CACHE='golfer-goodies-v2';
 const SHELL=['./','./index.html','./assets/css/styles.css','./assets/js/app.js','./assets/js/data-service.js','./assets/js/baseline.js','./manifest.webmanifest','./assets/icons/icon.svg','./data/courses.json','./data/products.json','./data/orders.json','./data/users.json','./data/promotions.json','./data/course-applications.json','./data/reviews.json','./data/rewards.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
